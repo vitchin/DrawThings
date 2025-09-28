a528fa59b835d665ef64d06b75578aa55039c495
Victor Ricardo Oliveira Nunes <victor12092004@gmail.com>
2025-09-28 16:32:15 -0300

    [feat] ajuste section hero

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,14 +1,13 @@
 import {WhatsappLogo, TagChevronIcon} from "@phosphor-icons/react/dist/ssr";
 import drawhand from "../../../public/hero.jpg";
 import percent from "../../../public/percent.png";
-import logo from "../../../public/logo.png";
 import Image from "next/image";
 
 
 
 export function Hero() {
     return (
-        <section className="bg-gradient-to-br from-[#775d3a] via-[#a88b5c] to-[#e2cba0] shadow-lg text-white relative overflow-hidden items-center justify-center flex flex-col h-fit">
+        <section className="bg-[#fffcf3] shadow-lg text-[#752f00] relative overflow-hidden items-center justify-center flex flex-col h-fit">
 
             <div className="absolute inset-0 w-full h-full">
                 <Image
@@ -17,15 +16,14 @@ export function Hero() {
                     fill
                     sizes="100%"
                     priority
-                    className="object-cover h-[300px] opacity-30 scale-x-[-1]"
+                    className="object-fill h-[300px] scale-x-[-1]"
                 />
-                <div className="absolute inset-0 bg-black opacity-40 md:hidden"></div>
             </div>
             
             <div className="container mx-auto pt-16 pb-16 px-4 relative">
 
-                <article className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                    <div className="space-y-12">
+                <article className="lg:w-[500px]">
+                    <div className="space-y-6">
                         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">
                             Inspire sua criatividade com os melhores materiais de desenho!
                         </h1>
@@ -34,36 +32,12 @@ export function Hero() {
                         </p>
                         <div>
                             <a href="#"
-                            className="bg-amber-900 shadow-md px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2">
-                                <WhatsappLogo className="w-5 h-5" />
+                            className="bg-[#e94d4d] text-white text-xl w-full h-16 shadow-md px-5 py-2 rounded-[50px] font-semibold flex items-center justify-center gap-2">
+                                <WhatsappLogo className="w-7 h-7" />
                                 Fale conosco no WhatsApp
                             </a>
                         </div>
 
-                        <div className="mt-8 mb-4 flex items-center gap-4">
-                            <div className="w-fit hidden lg:block">
-                                    <Image
-                                        src={percent}
-                                        alt="Imagem de um gato"
-                                        className="object-fill rounded-2xl"
-                                        quality={100}
-                                        width={100}
-                                    />
-                            </div>
-                            <p className="lg:text-lg mb-4"> de desconto na sua primeira compra</p>
-                        </div>
-                    </div>
-
-                     <div className="hidden md:block h-full relative">
-                        <Image
-                            src={logo}
-                            alt=""
-                            className="object-contain"
-                            fill
-                            sizes="(max-width: 768px) 0vw, 50vw"
-                            quality={100}
-                            priority
-                        />
                     </div>
 
                 </article>

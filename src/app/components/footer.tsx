bc488cefeba71eb8bd8d6642bac4df3603f2fe50
Victor Ricardo Oliveira Nunes <victor12092004@gmail.com>
2025-11-06 20:41:42 -0300

    [feat] ajustes footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,22 +1,6 @@
-import Image from "next/image";
-import golden from "../../../public/golden.png"
-import royal from "../../../public/primier.png"
-import primier from "../../../public/royal.png"
-import whiskas from "../../../public/natural.png"
-import natural from "../../../public/whiskas.png"
-
-
-const brands = [
-    {name: "Royal Canin", image: royal},
-    {name: "Golden", image: golden},
-    {name: "Primier", image: primier},
-    {name: "Formula Natural", image: natural},
-    {name: "Whiskas", image: whiskas}
-]
-
 export function Footer() {
     return (
-        <section id="SuportSection" className="bg-[#1b110e] py-16 text-white">
+        <section id="SuportSection" className="bg-[#0e0908] py-16 text-white">
             <div className="container mx-auto px-4">
                 <footer className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center justify-center">
                     <div>
@@ -31,9 +15,9 @@ export function Footer() {
                     <div>
                         <h4 className="text-lg font-semibold mb-4 text-[#FFD449]">Contato</h4>
                         <ul className="space-y-2 text-gray-200">
-                            <li>Email: contato@lojadearte.com.br</li>
+                            <li>Email: contatoExemplo@lojadearte.com.br</li>
                             <li>WhatsApp: (11) 99999-9999</li>
-                            <li>Endereço: Rua da Criatividade, 123 - São Paulo/SP</li>
+                            <li>Endereço: Rua exemplo, 123 - São Paulo/SP</li>
                         </ul>
                     </div>
                     <div>
